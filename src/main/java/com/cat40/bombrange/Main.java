58532b27fb20bfb18b8202dfc34d052b142a293d
cat40 <19844790+cat40@users.noreply.github.com>
2018-05-29 17:56:10 -0400

    added Deforestation Explosion

--- src/main/java/com/cat40/bombrange/Main.java
+++ src/main/java/com/cat40/bombrange/Main.java
@@ -22,6 +22,7 @@ import com.cat40.bombrange.blocks.cord.CordList;
 import com.cat40.bombrange.blocks.delay.*;
 import com.cat40.bombrange.blocks.dispenser.Dispenser;
 import com.cat40.bombrange.blocks.dynamite.Dynamite;
+import com.cat40.bombrange.blocks.explosives.DeforestationExplosive;
 import com.cat40.bombrange.blocks.explosives.MiningExplosive;
 import com.cat40.bombrange.blocks.fire.Fire;
 import com.cat40.bombrange.blocks.fire.FirePrime;
@@ -120,6 +121,7 @@ public static Block Tnt;
 public static Block Sandbag;
 public static Block Substrate;
 public static Block Mining;
+public static Block Deforestation;
 //public static Block StructOldCannon;
 
 public static Material stone = Material.rock;
@@ -303,6 +305,9 @@ event.manager.addSound("mod_id:Sparkler.ogg");
 
         Mining = new MiningExplosive(++idBase, bombMat, "MiningExplosive", C4Power).setStepSound(bombSound).setHardness(10.0F).setResistance(bombres);
         GameRegistry.registerBlock(Mining, "MiningExplosive");
+
+        Deforestation = new DeforestationExplosive(++idBase, bombMat, "DeforestationExplosive", C4Power).setStepSound(bombSound).setHardness(10.0F).setResistance(bombres);
+        GameRegistry.registerBlock(Deforestation, "DeforestationExplosive");
         
         /*FireLarge = new LargeFire(++idBase, bombMat, "FireLarge").setStepSound(bombSound).setHardness(10.0F).setResistance(bombres);
         GameRegistry.registerBlock(FireLarge, "FireLarge");*/
---
+++ src/main/java/com/cat40/bombrange/blocks/explosives/DeforestationExplosive.java
@@ -0,0 +1,21 @@
+package com.cat40.bombrange.blocks.explosives;
+
+import com.cat40.bombrange.explosions.DeforestationBoomBoom;
+import net.minecraft.block.material.Material;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.World;
+
+public class DeforestationExplosive extends Explosive {
+
+    public DeforestationExplosive(int id, Material material, String textureName, double power)
+    {
+        super(id, material, textureName, power);
+    }
+
+    public void blowUp(World world, int x, int y, int z)
+    {
+        Explosion boom = new DeforestationBoomBoom(world, x, y, z, (int) this.power);
+        boom.doExplosionA();
+        boom.doExplosionB(true);
+    }
+}
