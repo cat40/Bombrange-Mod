1458ec2d5ceea50260e4d55ba81dd41920881a81
cat40 <19844790+cat40@users.noreply.github.com>
2018-05-30 00:07:38 -0400

    added ActivatableExplosive class

---
+++ src/main/java/com/cat40/bombrange/blocks/explosives/ActivatableExplosive.java
@@ -0,0 +1,98 @@
+package com.cat40.bombrange.blocks.explosives;
+
+import com.cat40.bombrange.Main;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.world.World;
+
+public abstract class ActivatableExplosive extends Explosive {
+
+    public ActivatableExplosive(int id, Material material, String textureName, double power)
+    {
+        super(id, material, textureName, power);
+    }
+
+    @Override
+    public void onBlockAdded(World p_149726_1_, int p_149726_2_, int p_149726_3_, int p_149726_4_)
+    {
+        super.onBlockAdded(p_149726_1_, p_149726_2_, p_149726_3_, p_149726_4_);
+
+        if (p_149726_1_.isBlockIndirectlyGettingPowered(p_149726_2_, p_149726_3_, p_149726_4_))
+        {
+            this.onBlockDestroyedByPlayer(p_149726_1_, p_149726_2_, p_149726_3_, p_149726_4_, 1);
+            p_149726_1_.setBlockToAir(p_149726_2_, p_149726_3_, p_149726_4_);
+        }
+    }
+
+    @Override
+    public void onNeighborBlockChange(World p_149695_1_, int p_149695_2_, int p_149695_3_, int p_149695_4_, Block p_149695_5_)
+    {
+        if (p_149695_1_.isBlockIndirectlyGettingPowered(p_149695_2_, p_149695_3_, p_149695_4_))
+        {
+            this.onBlockDestroyedByPlayer(p_149695_1_, p_149695_2_, p_149695_3_, p_149695_4_, 1);
+            p_149695_1_.setBlockToAir(p_149695_2_, p_149695_3_, p_149695_4_);
+        }
+    }
+
+    @Override
+    public void onBlockDestroyedByPlayer(World p_149664_1_, int p_149664_2_, int p_149664_3_, int p_149664_4_, int p_149664_5_)
+    {
+        this.func_150114_a(p_149664_1_, p_149664_2_, p_149664_3_, p_149664_4_, p_149664_5_, (EntityLivingBase)null);
+    }
+
+    public void func_150114_a(World p_150114_1_, int p_150114_2_, int p_150114_3_, int p_150114_4_, int p_150114_5_, EntityLivingBase p_150114_6_)
+    {
+        if (!p_150114_1_.isRemote)
+        {
+            if ((p_150114_5_ & 1) == 1)
+            {
+               /* EntityTNTPrimed var7 = new EntityTNTPrimed(p_150114_1_, (double)((float)p_150114_2_ + 0.5F), (double)((float)p_150114_3_ + 0.5F), (double)((float)p_150114_4_ + 0.5F), p_150114_6_);
+                p_150114_1_.spawnEntityInWorld(var7);
+                p_150114_1_.playSoundAtEntity(var7, "game.tnt.primed", 1.0F, 1.0F);*/
+                this.primeTnt(p_150114_1_, p_150114_2_, p_150114_3_, p_150114_4_, 1);
+            }
+        }
+    }
+
+    /**
+     * Called upon block activation (right click on the block.)
+     */
+    @Override
+    public boolean onBlockActivated(World p_149727_1_, int p_149727_2_, int p_149727_3_, int p_149727_4_, EntityPlayer p_149727_5_, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_)
+    {
+        if (p_149727_5_.getCurrentEquippedItem() != null && p_149727_5_.getCurrentEquippedItem().getItem() == Main.Lighter)
+        {
+            //this.func_150114_a(p_149727_1_, p_149727_2_, p_149727_3_, p_149727_4_, 1, p_149727_5_);
+            p_149727_1_.setBlockToAir(p_149727_2_, p_149727_3_, p_149727_4_);
+            //p_149727_5_.getCurrentEquippedItem().damageItem(1, p_149727_5_);
+            this.primeTnt(p_149727_1_, p_149727_2_, p_149727_3_, p_149727_4_, 1);
+            return true;
+        }
+        else
+        {
+            return super.onBlockActivated(p_149727_1_, p_149727_2_, p_149727_3_, p_149727_4_, p_149727_5_, p_149727_6_, p_149727_7_, p_149727_8_, p_149727_9_);
+        }
+    }
+
+    @Override
+    public void onEntityCollidedWithBlock(World p_149670_1_, int p_149670_2_, int p_149670_3_, int p_149670_4_, Entity p_149670_5_)
+    {
+        if (p_149670_5_ instanceof EntityArrow && !p_149670_1_.isRemote)
+        {
+            EntityArrow var6 = (EntityArrow)p_149670_5_;
+
+            if (var6.isBurning())
+            {
+                //this.func_150114_a(p_149670_1_, p_149670_2_, p_149670_3_, p_149670_4_, 1, var6.shootingEntity instanceof EntityLivingBase ? (EntityLivingBase)var6.shootingEntity : null);
+                this.primeTnt(p_149670_1_, p_149670_2_, p_149670_3_, p_149670_4_, 1);
+                p_149670_1_.setBlockToAir(p_149670_2_, p_149670_3_, p_149670_4_);
+            }
+        }
+    }
+
+
+}
