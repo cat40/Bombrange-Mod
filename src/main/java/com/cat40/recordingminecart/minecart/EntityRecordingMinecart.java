429753c7ebe2d573dadab876905afb15659db121
cat40 <19844790+cat40@users.noreply.github.com>
2018-06-14 17:20:21 -0400

    added direction handling

--- src/main/java/com/cat40/recordingminecart/minecart/EntityRecordingMinecart.java
+++ src/main/java/com/cat40/recordingminecart/minecart/EntityRecordingMinecart.java
@@ -1,9 +1,14 @@
 package com.cat40.recordingminecart.minecart;
 
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.world.World;
 
 public class EntityRecordingMinecart extends EntityMinecart {
+
+    private double prevX, prevY, prevZ;
+    private Block ballast, left, right;
 
     public EntityRecordingMinecart(World world)
     {
@@ -30,11 +35,54 @@ public class EntityRecordingMinecart extends EntityMinecart {
         return (int) (cord-0.5);
     }
 
+    /**
+     * @return the direction the minecart is moving. does not check for vertical movement
+     */
+    private int getDirection()
+    {
+        if(this.prevX - this.posX >= 1)
+            return 1;
+        else if (this.prevX - this.posX <= -1)
+            return 2;
+        else if (this.prevZ - this.posZ >= 1)
+            return 3;
+        else if (this.prevZ - this.posZ <= -1)
+            return 4;
+        return -1;
+    }
+
     @Override
     public void onUpdate()
     {
         super.onUpdate();
-        
+        if(this.getDirection() > 0)
+        {
+            // do stuff
+            this.ballast = this.worldObj.getBlock(cordInt(this.posX), cordInt(this.posY-1), cordInt(this.posZ));
+            switch(getDirection())
+            {
+                case 1:
+                    left = this.worldObj.getBlock(cordInt(this.posX), cordInt(this.posY), cordInt(this.posZ-1));
+                    right = this.worldObj.getBlock(cordInt(this.posX), cordInt(this.posY), cordInt(this.posZ+1));
+                    break;
+                case 2:
+                    left = this.worldObj.getBlock(cordInt(this.posX), cordInt(this.posY), cordInt(this.posZ+1));
+                    right = this.worldObj.getBlock(cordInt(this.posX), cordInt(this.posY), cordInt(this.posZ-1));
+                    break;
+                case 3:
+                    left = this.worldObj.getBlock(cordInt(this.posX+1), cordInt(this.posY), cordInt(this.posZ));
+                    right = this.worldObj.getBlock(cordInt(this.posX-1), cordInt(this.posY), cordInt(this.posZ));
+                    break;
+                case 4:
+                    left = this.worldObj.getBlock(cordInt(this.posX-1), cordInt(this.posY), cordInt(this.posZ));
+                    right = this.worldObj.getBlock(cordInt(this.posX+1), cordInt(this.posY), cordInt(this.posZ));
+                    break;
+                default:
+                    throw new ValueException("direction " + getDirection() + " makes no sense");
+            }
+            this.prevX = this.posX;
+            this.prevY = this.posY;
+            this.prevZ = this.posZ;
+        }
     }
-
 }
